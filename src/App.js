6041cbbbe83de99161bdf7dbb5a00664a27a3393
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-08-08 20:24:58 -0400

    created dictionary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from "./CJlogo.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} className="App-logo img-fluid" alt="logo" />
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Courtney Jones</small>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
