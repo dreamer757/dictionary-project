a16921cb3ef1b17807b73987bd611ee1d83e6161
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-08-16 03:39:19 -0400

    fixed typo added more image rows

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary(props) {
 
     let pexelsApiKey =
       "563492ad6f91700001000001d5333cc1e9244da88f25dedc653a2c4b";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
